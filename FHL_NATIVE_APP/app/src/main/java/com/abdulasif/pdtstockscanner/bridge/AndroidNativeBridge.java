59a1e7ad10168906c1673b0356a141b8a251c213
aasif010656-web <3.00652637e+08+aasif010656-web@users.noreply.github.com>
2026-08-14 13:00:07 +0000

    Fix installer bridge logging import

--- FHL_NATIVE_APP/app/src/main/java/com/abdulasif/pdtstockscanner/bridge/AndroidNativeBridge.java
+++ FHL_NATIVE_APP/app/src/main/java/com/abdulasif/pdtstockscanner/bridge/AndroidNativeBridge.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.util.Base64;
+import android.util.Log;
 import android.webkit.JavascriptInterface;
 
 import androidx.activity.result.ActivityResultLauncher;
